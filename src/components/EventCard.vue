ee790c180b66c4eb951ab962cb6eb0c8ab650210
Daniel Schmid <daniel.schmid@mailbox.org>
2022-10-08 18:16:54 +0200

    feat:

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -10,10 +10,12 @@ export default {
 </script>
 
 <template>
-  <div class="event-card">
-    <span>@{{ event.time }} on {{ event.date }}</span>
-    <h4>{{ event.title }}</h4>
-  </div>
+  <router-link :to="{ name: 'EventDetails', params: { id: event.id } }">
+    <div class="event-card">
+      <span>@{{ event.time }} on {{ event.date }}</span>
+      <h4>{{ event.title }}</h4>
+    </div>
+  </router-link>
 </template>
 
 <style scoped>
