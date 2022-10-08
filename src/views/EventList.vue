a5bb8b9eef91c19a2513c4a61048adca406ebc46
Daniel Schmid <daniel.schmid@mailbox.org>
2022-10-08 21:12:19 +0200

    feat: building pagination

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,23 +1,36 @@
 <script>
+import { watchEffect } from "vue";
 import EventCard from "../components/EventCard.vue";
 import EventService from "../services/EventService";
 
 export default {
   name: "EventList",
+  props: ["page"],
   components: { EventCard },
   data() {
     return {
       events: null,
+      totalEvents: 0,
     };
   },
   created() {
-    EventService.getEvents()
-      .then((response) => {
-        this.events = response.data;
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    watchEffect(() => {
+      this.events = null;
+      EventService.getEvents(2, this.page)
+        .then((response) => {
+          this.events = response.data;
+          this.totalEvents = response.headers["x-total-count"];
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    });
+  },
+  computed: {
+    hasNextPage() {
+      var totalPages = Math.ceil(this.totalEvents / 2);
+      return this.page < totalPages;
+    },
   },
 };
 </script>
@@ -27,6 +40,24 @@ export default {
 
   <main class="events">
     <EventCard v-for="event in events" :key="event.id" :event="event" />
+
+    <div class="pagination">
+      <router-link
+        id="page-prev"
+        :to="{ name: 'EventList', query: { page: page - 1 } }"
+        rel="prev"
+        v-if="page != 1"
+        >&#60; Previous</router-link
+      >
+
+      <router-link
+        id="page-next"
+        :to="{ name: 'EventList', query: { page: page + 1 } }"
+        rel="next"
+        v-if="hasNextPage"
+        >Next &#62;</router-link
+      >
+    </div>
   </main>
 </template>
 
@@ -40,5 +71,11 @@ h1 {
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+
+.pagination {
+  display: flex;
+  justify-content: space-around;
+  width: 100%;
 }
 </style>
