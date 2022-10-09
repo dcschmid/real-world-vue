a749841c464d8f1ccc9f37fa88a64196b41ab244
Daniel Schmid <daniel.schmid@mailbox.org>
2022-10-09 22:20:44 +0200

    fix: scroll behavior

--- src/router/index.js
+++ src/router/index.js
@@ -83,6 +83,13 @@ const router = createRouter({
       component: NotFound,
     },
   ],
+  scrollBehavior(_to, _from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    } else {
+      return { top: 0 };
+    }
+  },
 });
 
 router.beforeEach(() => {
